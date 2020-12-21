9ae1738e71de5e28a70dfbd51de93bf1a708f8cb
MarianaGomes-UPskill <marianaribgomes@upskill.pt>
2020-12-21 11:14:32 +0000

    Context added

---
+++ SafetyTourismApi/SafetyTourismApi/Data/WHOContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SafetyTourismApi.Data
+{
+    public class WHOContext : DbContext
+    {
+            public WHOContext(DbContextOptions<WHOContext> options)
+                : base(options)
+            {
+            }
+
+            public DbSet<WHOContext> TodoItems { get; set; }
+        }
+    }
--- SafetyTourismApi/SafetyTourismApi/Startup.cs
+++ SafetyTourismApi/SafetyTourismApi/Startup.cs
@@ -1,16 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SafetyTourismApi.Data;
 
 namespace SafetyTourismApi
 {
@@ -27,11 +21,11 @@ namespace SafetyTourismApi
         public void ConfigureServices(IServiceCollection services)
         {
 
+             services.AddDbContext<WHOContext>(opt =>
+                                               opt.UseInMemoryDatabase("WHOList"));
+
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SafetyTourismApi", Version = "v1" });
-            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,8 +34,6 @@ namespace SafetyTourismApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SafetyTourismApi v1"));
             }
 
             app.UseHttpsRedirection();
