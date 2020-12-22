75dce9b0f758373f79b27eecd61073b9c80e1b67
MarianaGomes-UPskill <marianaribgomes@upskill.pt>
2020-12-22 09:51:03 +0000

    AuthenticateRequest and AuthenticateResponse Class Added

---
+++ SafetyTourismApi/SafetyTourismApi/Models/AuthenticateRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SafetyTourismApi.Models
+{
+    public class AuthenticateRequest
+    {
+    }
+}
---
+++ SafetyTourismApi/SafetyTourismApi/Models/AuthenticateResponse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Providers.Entities;
+
+namespace SafetyTourismApi.Models
+{
+    public class AuthenticateResponse
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Token { get; set; }
+
+        public AuthenticateResponse(User user, string token)
+        {
+            Id = user.Id;
+            FirstName = user.FirstName;
+            LastName = user.LastName;
+            UserName = user.UserName;
+            Token = token;
+        }
+
+    }
+}
