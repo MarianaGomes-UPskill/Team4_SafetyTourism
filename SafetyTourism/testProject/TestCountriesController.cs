1d9a9dcd8d02f43008267e4c2901b68362b61327
TiagoAzevedoUpSkill <tiagomina.azevedo@upskill.pt>
2021-01-16 21:10:32 +0000

    PUT Test added

--- SafetyTourism/testProject/TestCountriesController.cs
+++ SafetyTourism/testProject/TestCountriesController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SafetyTourismApi.Controllers;
 using SafetyTourismApi.Data;
 using SafetyTourismApi.Models;
@@ -34,5 +35,56 @@ namespace testProject
             var items = Assert.IsType<Country>(result.Value);
             Assert.Equal("Australia", items.CountryName);
         }
+
+        [Fact]
+        public async Task PutCountry_ShouldReturnEditedCountry()
+        {
+            var TestContext = TodoContextMocker.GetWHOContext("PutCountry");
+            var theController = new CountriesController(TestContext);
+
+            var newCountry = new Country
+            {
+                CountryID = 1,
+                CountryName = "China",
+                GeoZoneID = 1
+            };
+
+            var oldCountryResult = await theController.GetCountry(1);
+            var oldCountry = oldCountryResult.Value;
+
+            TestContext.Entry(oldCountry).State = EntityState.Detached;
+
+            var result = await theController.PutCountry(newCountry.CountryID, newCountry);
+            var getResult = await theController.GetCountry(newCountry.CountryID);
+
+            var items = Assert.IsType<Country>(getResult.Value);
+            Assert.Equal("China", items.CountryName);
+            Assert.IsType<NoContentResult>(result); 
+        }
+
+        [Fact]
+        public async Task PostCountry_ShouldCreateNewCountry()
+        {
+            var TestContext = TodoContextMocker.GetWHOContext("PostCountry");
+            var theController = new CountriesController(TestContext);
+
+            var newCountry = new Country
+            {
+                CountryID = 3,
+                CountryName = "Islândia",
+                GeoZoneID = 1
+            };
+            var addedCountryResult = await theController.GetCountry(3);
+            var addedCountry = addedCountryResult.Value;
+
+            TestContext.Entry(addedCountry).State = EntityState.Detached;
+
+            var result = await theController.PostCountry(newCountry);
+            var items = Assert.IsType<Country>(result.Value);
+            Assert.Equal("Islândia", items.CountryName);
+            Assert.IsType<CreatedAtActionResult>(result);
+        }
+
+
     }
 }
--- SafetyTourismApi/SafetyTourismApi/Models/Country.cs
+++ SafetyTourismApi/SafetyTourismApi/Models/Country.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +9,8 @@ namespace SafetyTourismApi.Models
 {
     public class Country
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int CountryID { get; set; }
 
         public string CountryName { get; set; }
