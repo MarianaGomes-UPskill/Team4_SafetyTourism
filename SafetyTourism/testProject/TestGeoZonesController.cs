98924f9ad39e8376b9303228229a0cf6e3bd9917
MarianaGomes-UPskill <marianaribgomes@upskill.pt>
2021-01-18 16:21:58 +0000

    Get, Put and Post tests added to TestGeoZonesController

--- SafetyTourism/testProject/TestGeoZonesController.cs
+++ SafetyTourism/testProject/TestGeoZonesController.cs
@@ -17,7 +17,7 @@ namespace testProject
         [Fact]
         public async Task GetAllGeoZonesAsync_ShouldReturnAllGeoZones()
         {
-            Thread.Sleep(3000);
+            Thread.Sleep(2000);
             var TestContext = TodoContextMocker.GetWHOContext("GetAllGeoZones");
             var theController = new GeoZonesController(TestContext);
 
@@ -25,6 +25,17 @@ namespace testProject
 
             var items = Assert.IsType<List<GeoZone>>(result.Value);
             Assert.Equal(2, items.Count);
+        }
+
+        [Fact]
+        public async Task GetGeoZoneAsync_ShouldReturnNotFound()
+        {
+            var TestContext = TodoContextMocker.GetWHOContext("GeoZonesReturnNotFound");
+            var theController = new GeoZonesController(TestContext);
+
+            var response = await theController.GetGeoZoneByID(6);
+
+            Assert.IsType<NotFoundResult>(response.Result);
         }
 
         [Fact]
@@ -39,6 +50,22 @@ namespace testProject
             var items = Assert.IsType<GeoZone>(result.Value);
             Assert.Equal("Oceania", items.GeoZoneName);
         }
+
+        [Fact]
+        public async Task GetGeoZoneByID_ShouldReturnTheRightItemAsync()
+        {
+            var TestContext = TodoContextMocker.GetWHOContext("GetGeoZoneReturnRightItem");
+            var theController = new GeoZonesController(TestContext);
+
+            var result = await theController.GetGeoZoneByID(1);
+            var items = result.Value;
+
+            //Assert     
+            Assert.IsType<GeoZone>(items);
+            Assert.Equal(1, items.GeoZoneID);
+        }
+
+
         [Fact]
         public async Task PutGeoZone_ShouldReturnEditedGeoZone()
         {
@@ -64,6 +91,41 @@ namespace testProject
             Assert.Equal("Asia", items.GeoZoneName);
             Assert.IsType<NoContentResult>(result);
         }
+
+        [Fact]
+        public async Task PutNoExistingGeoZoneAsync_ShouldReturnNotFound()
+        {
+            Thread.Sleep(4000);
+            var TestContext = TodoContextMocker.GetWHOContext("EditGeoZone_ShouldReturnNotFound");
+            var theController = new GeoZonesController(TestContext);
+            var theNonExistingGeoZone = new GeoZone
+            {
+                GeoZoneID = 5,
+                GeoZoneName = "Updated GeoZone"
+            };
+
+            var response = await theController.PutGeoZone(5, theNonExistingGeoZone);
+
+            Assert.IsType<NotFoundResult>(response);
+        }
+
+        //[Fact]
+        //public async Task PutBadNameGeoZoneAsync_ShouldReturnBadRequest()
+        //{
+        //    Thread.Sleep(4000);
+        //    var TestContext = TodoContextMocker.GetWHOContext("EditGeoZone_ShouldReturnBadRequest");
+        //    var theController = new GeoZonesController(TestContext);
+        //    var theNonExistingGeoZone = new GeoZone
+        //    {
+        //        GeoZoneID = 5,
+        //    };
+
+        //    var g = await TestContext.FindAsync<GeoZone>(5);
+
+        //    var response = await theController.PutGeoZone(5, theNonExistingGeoZone);
+
+        //    Assert.IsType<BadRequestResult>(response);
+        //}
 
         [Fact]
         public async Task PostGeoZone_ShouldCreateNewGeoZone()
@@ -92,7 +154,21 @@ namespace testProject
             var result = await theController.DeleteGeoZone(1);
             var getresult = await theController.GetGeoZoneByID(1);
 
-            Assert.IsType<NoContentResult>(result);
+            Assert.IsType<NotFoundResult>(getresult.Result);
         }
+
+        [Fact]
+        public async Task DeleteExistingGeoZoneAsync_ShouldReturnOkResult()
+        {
+            Thread.Sleep(4000);
+            var TestContext = TodoContextMocker.GetWHOContext("DeleteGeoZone_OkResult");
+            var theController = new GeoZonesController(TestContext);
+            
+            var response = await theController.DeleteGeoZone(2);
+
+            Assert.IsType<NoContentResult>(response);
+        }
+
+
     }
 }
