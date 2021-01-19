2eb07576019aa9c198cbcce9c36eec387dca6d44
caio_reis <caiocruzeiror@upskill.pt>
2021-01-19 17:29:29 +0000

    Date time fixed

--- SafetyTourismApi/SafetyTourismApi/Data/DBInitializer.cs
+++ SafetyTourismApi/SafetyTourismApi/Data/DBInitializer.cs
@@ -84,18 +84,18 @@ namespace SafetyTourismApi.Data {
 
             var outbreaks = new OutBreak[]
             {
-                    new OutBreak{GeoZoneID=1, VirusID=3, StartDate= new DateTime(2020-02-22)},
-                    new OutBreak{GeoZoneID=2, VirusID=2, StartDate= new DateTime(2021-10-02)},
-                    new OutBreak{GeoZoneID=2, VirusID=1, StartDate= new DateTime(2022-11-11)},
-                    new OutBreak{GeoZoneID=2, VirusID=8, StartDate= new DateTime(2012-03-02)},
-                    new OutBreak{GeoZoneID=3, VirusID=7, StartDate= new DateTime(2000-05-06), EndDate= new DateTime(2009-08-20)},
-                    new OutBreak{GeoZoneID=4, VirusID=5, StartDate= new DateTime(2050-06-01)},
-                    new OutBreak{GeoZoneID=7, VirusID=9, StartDate= new DateTime(2003-08-02)},
-                    new OutBreak{GeoZoneID=8, VirusID=6, StartDate= new DateTime(2004-02-28)},
-                    new OutBreak{GeoZoneID=4, VirusID=11, StartDate= new DateTime(1995-01-21)},
-                    new OutBreak{GeoZoneID=7, VirusID=9, StartDate= new DateTime(1950-09-30)},
-                    new OutBreak{GeoZoneID=8, VirusID=10, StartDate= new DateTime(1989-10-25)},
-                    new OutBreak{GeoZoneID=9, VirusID=12, StartDate= new DateTime(1993-01-29), EndDate= new DateTime(1999-01-02)}
+                    new OutBreak{GeoZoneID=1, VirusID=3, StartDate= DateTime.Parse("2020-02-22")},
+                    new OutBreak{GeoZoneID=2, VirusID=2, StartDate= DateTime.Parse("2021-10-02")},
+                    new OutBreak{GeoZoneID=2, VirusID=1, StartDate= DateTime.Parse("2022-11-11")},
+                    new OutBreak{GeoZoneID=2, VirusID=8, StartDate= DateTime.Parse("2012-03-02")},
+                    new OutBreak{GeoZoneID=3, VirusID=7, StartDate= DateTime.Parse("2000-05-06"), EndDate= DateTime.Parse("2009-08-20")},
+                    new OutBreak{GeoZoneID=4, VirusID=5, StartDate= DateTime.Parse("2050-06-01")},
+                    new OutBreak{GeoZoneID=7, VirusID=9, StartDate= DateTime.Parse("2003-08-02")},
+                    new OutBreak{GeoZoneID=8, VirusID=6, StartDate= DateTime.Parse("2004-02-28")},
+                    new OutBreak{GeoZoneID=4, VirusID=11,StartDate= DateTime.Parse("1995-01-21")},
+                    new OutBreak{GeoZoneID=7, VirusID=9, StartDate= DateTime.Parse("1950-09-30")},
+                    new OutBreak{GeoZoneID=8, VirusID=10,StartDate= DateTime.Parse("1989-10-25")},
+                    new OutBreak{GeoZoneID=9, VirusID=12,StartDate= DateTime.Parse("1993-01-29"), EndDate= DateTime.Parse("1999-01-02")}
             };
             foreach (OutBreak o in outbreaks)
             {
@@ -105,9 +105,9 @@ namespace SafetyTourismApi.Data {
 
             var recomendation = new Recomendation[]
 {
-                    new Recomendation{Note="Be careful", GeoZoneID=3, CreationDate= new DateTime(2001-02-22), ExpirationDate=20},
-                    new Recomendation{Note="Don't drive", GeoZoneID=5, CreationDate=new DateTime(1996-12-22), ExpirationDate=1500},
-                    new Recomendation{Note="Kill it with fire", GeoZoneID=6, CreationDate=new DateTime(2008-02-25), ExpirationDate=260}
+                    new Recomendation{Note="Be careful", GeoZoneID=3, CreationDate= DateTime.Parse("2001-02-22"), ExpirationDate=20},
+                    new Recomendation{Note="Don't drive", GeoZoneID=5, CreationDate= DateTime.Parse("1996-12-22"), ExpirationDate=1500},
+                    new Recomendation{Note="Kill it with fire", GeoZoneID=6, CreationDate= DateTime.Parse("2008-02-25"), ExpirationDate=260}
 
     };
             foreach (Recomendation r in recomendation)
